838d5ee04bcb7545f8d6aae61adb54d7d86fd9ca
Moaaz Ibrahim <75554068+moaaz-ibrahim@users.noreply.github.com>
2022-03-10 07:20:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.post('/register' , (req,res)=>{
     if (username.includes(`"`) || password.includes(`"`) || mail.includes(`"`)){
         res.json(0)
     }
-  else if (username.includes(``) || password.includes(``) || mail.includes(``))res.json(0)
+    else if (username.includes(``) || password.includes(``) || mail.includes(``))res.json(0)
     else{
         var reg = database.register(username , password , mail);
         if (reg)
@@ -33,13 +33,25 @@ app.post('/register' , (req,res)=>{
     
 
 })
+
 app.post('/login' , (req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
-    var log  = database.login(username , password);
-    if (log == 1)
-    res.json(1)
-    else res.json(0)
+    const db = database.readDb();
+    const jdb = JSON.parse(db)[0];
+    if (username == jdb.username && password==jdb.password){
+       res.json({
+           response : 2,
+           data : db
+       })
+    }
+    else {
+
+        var log  = database.login(username , password);
+        if (log == 1)
+        res.json(1)
+        else res.json(0)
+    }
     
 })
 
